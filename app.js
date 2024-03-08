f4042a128bf31abadc48d3d57f01353b5952fa79
Sasori Morningstar <medjedoub.ilyes07@gmail.com>
2024-03-08 12:57:18 +0100

    prod

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ require("dotenv").config()
 
 const app = express()
 
-app.listen(process.env.PORT, () => {
+/*app.listen(process.env.PORT, () => {
     console.log("Listening on port " + process.env.PORT)
-})
+})*/
 
-app.use("/api", routes)
+//app.use("/api", routes)
 
 app.get("/", async (req, res) => {
-    res.status(200)
     res.send("Welcome to your email visualiser")
 })
 
